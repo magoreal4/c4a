ff25671775384ec913b6187a8962c92463a2024a
magoreal <magoreal4@gmail.com>
2022-11-20 19:50:08 -0400

    first commit

--- home/index-home.js
+++ home/index-home.js
@@ -19,6 +19,9 @@ document.addEventListener("DOMContentLoaded", function () {
   //  width       = Math.max($(window).width(), window.innerWidth),
   //  mobileTest  = false;
 
+  // var textWrapper = document.querySelector('.commons');
+  // textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
   $(window).scroll(function () {
     effectsHomeSection(homeSection, this);
     // navbarAnimation(navbar, homeSection, navHeight);
@@ -57,8 +60,51 @@ document.addEventListener("DOMContentLoaded", function () {
       translateX: ["200%", 0],
       duration: 4000,
     },
-    "-=4000"
-  );
+    "-=4000");
+  // tl.add({
+  //   targets: ' .letter',
+  //   translateX: [-400,0],
+  //   translateZ: 0,
+  //   // opacity: [0,1],
+  //   easing: "easeOutExpo",
+  //   duration: 1200,
+  //   delay: (el, i) => 500 + 30 * i
+  // },
+  // "-=2000");
+
+  var textcommons = document.querySelector('.commons');
+  var textall = document.querySelector('.all');
+  textcommons.innerHTML = textcommons.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  textall.innerHTML = textall.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  
+  var tl2 = anime.timeline();
+  tl2.add({
+      targets: '.commons .letter',
+      translateX: [-400,0],
+      translateZ: 0,
+      opacity: [0,1],
+      easing: "easeOutExpo",
+      duration: 3500,
+      delay: (el, i) => 500 - 60 * i
+    });
+    tl2.add({
+      targets: '.all .letter',
+      translateX: [+400,0],
+      translateZ: 0,
+      opacity: [0,1],
+      easing: "easeOutExpo",
+      duration: 3500,
+      delay: (el, i) => -500 + 60 * i
+    }, "-=3500");
+    tl2.add({
+      targets: '.four',
+      translateZ: 0,
+      opacity: [0,1],
+      scale: [0,1],
+      easing: "easeOutExpo",
+      duration: 3000
+    },"-=2000");
+
 
   // tl.add({
   // targets: '.logoEffects .texto',
